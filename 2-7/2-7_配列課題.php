ce584f69341a15c0a4001912dda75d2a757580ef
hashi-an <annamsrp@gmail.com>
2023-02-20 00:11:39 +0900

    課題提出 2-7
    配列 連想配列

---
+++ 2-7/2-7_配列課題.php
@@ -0,0 +1,13 @@
+<?php
+
+//連想配列 3項目
+$arr_col = ["red" => "赤", "blue" => "青", "green" => "緑"]; 
+var_dump($arr_col);
+echo '<br>';
+
+//連想配列 3項目+黄色
+$arr_col["yellow"] = "黄色";
+var_dump($arr_col);
+
+
+?>
